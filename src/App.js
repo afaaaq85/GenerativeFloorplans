5124513eeca8a8ff3725b5d60cf947ea0ce75437
Afaq Ahmed <afaaaq@gmail.com>
2023-02-27 19:29:26 +0500

    committing till banner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/Navbar/NavBar';
 import Banner from './Components/Banner/Banner';
+import Skills from './Components/Skills/Skills';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className='App'>
       <NavBar/>
       <Banner/>
+      <Skills></Skills>
     </div>
   );
 }
