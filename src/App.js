d565138e728b31f8cb483502230e53cf7da0bb82
Afaq Ahmed <mughalfasih75@gmail.com>
2023-02-28 11:46:18 +0500

    final

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Skills from './Components/Skills/Skills';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Projects from './Components/Projects/Projects';
 import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Skills/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
